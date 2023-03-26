1950896859455055d3865e20eb7c01dadebda46c
DarkMindpd <pedram.azini@gmail.com>
2023-03-26 18:17:06 +0430

    refresh in eyd

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=G:\python bamdad\vsc venv\.venv
+set VIRTUAL_ENV=D:\Python Bamdad\django-project-One\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
