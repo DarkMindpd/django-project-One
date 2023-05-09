acc60f2a538cae6c558405b5635d6619bd3fabe7
DarkMindpd <pedram.azini@gmail.com>
2023-05-09 17:30:15 +0330

    new edit 2.

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('account', views.enter_acc, name='enter_acc'),
     path('register_successful', TemplateView.as_view(template_name = 'accounts\index_success.html'), name='successful_singup'),
     path('Logout_successful', views.log_out, name='successful_logout'),
-    path('<str:username>/profile', views.profile_detail, name='profile_detail'),
+    path('profile/<str:username>', views.profile_detail, name='profile_detail'),
     path('followToggle/<str:author>', views.followToggle, name='followToggle')
 
 ]
