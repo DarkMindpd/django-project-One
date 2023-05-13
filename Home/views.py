ce8c8d1ee132a42be8468391db8759124f703569
DarkMindpd <pedram.azini@gmail.com>
2023-05-13 20:09:49 +0330

    new update.

--- Home/views.py
+++ Home/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from post.models import Post
 from django.contrib.auth.decorators import login_required
+from django.template.defaultfilters import register
+
 
 def Home(request):
      p = Post.objects.all()
-     l = []
+     pl = {}
      for i in p:
           a = i.like.filter().order_by('-id')[:2][::-1]
-          l.append(a)
-     
-     return render(request, 'Home\home.html', {"post" : p, "like" : l})
+          pl[i] = a
+     print(pl.get)
+     return render(request, 'Home\home.html', {"post" : pl})
 
+@register.filter
+def get_item(dictionary, key):
+    return dictionary.get(key)
 
 
