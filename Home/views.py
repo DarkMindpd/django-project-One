6299995e4fc8d1e2acd69055cc497f4a1fb5e097
DarkMindpd <pedram.azini@gmail.com>
2023-05-28 10:05:53 +0330

    new update img upload

--- Home/views.py
+++ Home/views.py
@@ -4,10 +4,10 @@ from django.contrib.auth.decorators import login_required
 
 
 def Home(request):
-     p = Post.objects.all()
+     p = Post.objects.all().order_by('-id')
      pl = {}
      for i in p:
-          a = i.like.filter().order_by('-id')[:4][::-1]
+          a = i.like.filter().order_by('-id')[:4]
           pl[i] = a
      return render(request, 'Home\home.html', {"post" : pl})
 
